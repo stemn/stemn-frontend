6d3be60cc2c7009535ae303cb946fe2f08e10661
DaveRevay2 <daverevay@gmail.com>
2016-10-17 15:37:41 +1100

    web gerber semi-working

--- app/renderer/assets/other/gerber/js/constants/webGerberConstants.js
+++ app/renderer/assets/other/gerber/js/constants/webGerberConstants.js
@@ -1,19 +1,7 @@
-(function (root, factory) {
-    if (typeof define === "function" && define.amd) {
-        define(function () {
-            return factory();
-        });
-    } else if (typeof module === "object" && module.exports) {
-        module.exports = factory();
-    } else {
-        root.webGerberConstants = factory();
-    }
-}(this, function () {
-  return {
-      colors: {
-          board      : '#103410',
-          boardDark  : '#0a1f0a',
-          boardLight : '#133b13',
-      }
+export default {
+  colors: {
+    board      : '#103410',
+    boardDark  : '#0a1f0a',
+    boardLight : '#133b13',
   }
-}))
+}
