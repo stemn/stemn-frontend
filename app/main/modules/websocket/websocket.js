16607ad8ccaadfa0a5ec9f31b6c4364cceac76fb
Jackson Delahunt <jackson@stemn.com>
2016-10-27 21:52:23 +1100

    primus seems to be working

--- app/main/modules/websocket/websocket.js
+++ app/main/modules/websocket/websocket.js
@@ -1,16 +1,9 @@
 import primus from '../../../primus-websockets.js';
 import ws from 'ws';
 
-console.log('ws', !!ws);
-console.log('primus', !!primus);
-
-
 export const initialise = (config) => {
 
   const socket = primus.connect(`${config.host}:${config.port}`);
-
-//  console.log('primus', socket);
-//  const socket = new Socket(`${config.host}:${config.port}`);
 
   const socketError = (err) => socket.write({
     type : 'log',
