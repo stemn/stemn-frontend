40b998dcdae01884511a69868dd09be72b72f95c
Jackson Delahunt <jackson@stemn.com>
2016-10-31 22:33:11 +1100

    move socket error

--- app/main/modules/websocket/websocket.js
+++ app/main/modules/websocket/websocket.js
@@ -7,21 +7,19 @@ export const initialise = (config) => {
 
   socket = primus.connect(`${config.host}:${config.port}`);
 
-  const socketError = (err) => socket.write({
-    type : 'log',
-    payload : {
-      type : 'error',
-      message : err.message
-    }
-  });
-
   const oldWrite = socket.write.bind(socket);
   socket.write = (data) => {
       console.log('WRITING DATA\n', JSON.stringify(data, null, 4));
       return oldWrite(data);
   }
 
-  socket.on('error', socketError);
+  socket.on('error', (err) => socket.write({
+    type : 'ADMIN/LOG',
+    payload : {
+      type : 'error',
+      message : err.message
+    }
+  });
 
   socket.on('data', (data) => console.log('WEBSOCKET RECEIVED DATA:\n', JSON.stringify(data)));
 
