9098b27f8cfa03a28e1cae76fa80fef1591ba664
DaveRevay2 <daverevay@gmail.com>
2016-12-07 11:31:47 +1100

    Made the release notes work

--- app/renderer/main/modules/Modal/modals/ReleaseNotesModal.jsx
+++ app/renderer/main/modules/Modal/modals/ReleaseNotesModal.jsx
@@ -1,20 +1,46 @@
 // Component Core
 import React from 'react';
-import releaseNotes from 'raw!app/release-notes.md';
+import http from 'axios';
+import { version } from 'app/package.json'
 
 // Styles
 import classNames from 'classnames';
 
 import Button from 'app/renderer/main/components/Buttons/Button/Button'
 import EditorDisplay from 'app/renderer/main/modules/Editor/EditorDisplay.jsx';
+import LoadingOverlay from 'app/renderer/main/components/Loading/LoadingOverlay/LoadingOverlay.jsx';
 
 const Component = React.createClass({
+  getInitialState () {
+    return {
+      releaseNotes: '',
+      loading: true
+    }
+  },
+  componentWillMount() {
+    http({
+      url: `https://api.github.com/repos/stemn/stemn-desktop/releases/tags/v${version}`,
+      headers: {
+        Authorization: null
+      }
+    }).then(response => {
+      this.setState({
+        releaseNotes: `## Version ${version} \r\n ${response.data.body}\r\n\r\n [View All Release Notes](https://github.com/Stemn/Stemn-Desktop/releases)` ,
+        loading: false
+      })
+    }).then(response => {
+      this.setState({ loading: false })
+    })
+  },
   render: function() {
     const { modalHide } = this.props;
+    const { loading, releaseNotes} = this.state;
+
     return (
-      <div style={{width: '700px'}}>
+      <div style={{width: '600px'}}>
         <div className="modal-title">Stemn Release Notes</div>
-        <div className="modal-body">
+        <div className="modal-body rel-box" style={{minHeight: '200px', paddingTop: '0px'}}>
+          <LoadingOverlay show={loading}/>
           <EditorDisplay value={releaseNotes}/>
         </div>
         <div className="modal-footer-no-line layout-row layout-align-end">
--- app/renderer/shared/standardSetup.js
+++ app/renderer/shared/standardSetup.js
@@ -15,7 +15,11 @@ export default (store, rendererType) => {
   // Setup the http interceptor
   http.interceptors.request.use(config => {
     const token = store.getState().auth.authToken;
-    if (!config.headers.Authorization) {
+
+    if(config.headers.Authorization === null){// We set Authorization to null to bypass
+      delete config.headers.Authorization
+    }
+    else if(!config.headers.Authorization) {
       config.headers.Authorization = token ? `bearer ${token}` : ''
     }
     return config;
