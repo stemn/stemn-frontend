1d72a65c7750558736cfdfc533af24c817d7826a
DaveRevay2 <daverevay@gmail.com>
2016-12-14 16:41:04 +1100

    fixed fullPath bug - seems to be a reserved word?

--- app/renderer/main/modules/Files/Files.actions.js
+++ app/renderer/main/modules/Files/Files.actions.js
@@ -118,21 +118,20 @@ export function getFullPath({ path, projectId, provider }) {
     const addSlash = (path) => {
       return path && path[0] != '/' && path[0] != '\\' ? '/' + path : path
     }
-    const normaliseSlashes = (path) => {
-      return path.replace('/', '\\')
-    }
     return new Promise((resolve, reject) => {
       const computerToProvider = storeState.system.providerPath[provider];
       const projectToFile      = path;
-
+      const normaliseSlashes = (path) => {
+        return path.replace('/', '\\')
+      }
       if(has(storeState, [localPathModuleName, projectId, 'data'])){
-        const fullPath = computerToProvider + addSlash(storeState[localPathModuleName][projectId].data) + addSlash(projectToFile);
-        resolve(normaliseSlashes(fullPath))
+        const completePath = computerToProvider + addSlash(storeState[localPathModuleName][projectId].data) + addSlash(projectToFile);
+        resolve(normaliseSlashes(completePath))
       }
       else{
         dispatch(LocalPathActions.getPath({provider, projectId})).then(response => {
-          const fullpath = computerToProvider + addSlash(response.value.data) + addSlash(projectToFile);
-          resolve(normaliseSlashes(fullPath))
+          const completePath = computerToProvider + addSlash(response.value.data) + addSlash(projectToFile);
+          resolve(normaliseSlashes(completePath))
         }).catch(reject)
       }
     })
