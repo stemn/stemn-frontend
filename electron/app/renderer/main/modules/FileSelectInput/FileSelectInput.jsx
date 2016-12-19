b7357c1e6a519188c2a2d55150d2971b1e2c4c13
DaveRevay2 <daverevay@gmail.com>
2016-12-19 11:20:44 +1100

    refactor

---
+++ electron/app/renderer/main/modules/FileSelectInput/FileSelectInput.jsx
@@ -0,0 +1,82 @@
+import React, { PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+// Container Actions
+import * as ModalActions from 'app/renderer/main/modules/Modal/Modal.actions.js';
+
+import classes from './FileSelectInput.css'
+import classNames from 'classnames';
+import MdFolder from 'react-icons/md/folder';
+import SimpleIconButton from 'app/renderer/main/components/Buttons/SimpleIconButton/SimpleIconButton'
+import { isDriveFileId, isDropboxFileId } from 'app/shared/modules/Files/Files.utils.js';
+
+
+const propTypesObject = {
+  projectId: PropTypes.string,
+  provider: PropTypes.string.isRequired,    // 'dropbox' || 'drive' - The provider
+  model: PropTypes.string.isRequired,       // The model to assign the selected file
+  value: PropTypes.object.isRequired,       // The value of the selected file: { path, fileId }
+  disabled: PropTypes.bool                  // Should we disable the input
+};
+
+const FileSelectInput = React.createClass({
+  showModal(){
+    this.props.ModalActions.showModal({
+      modalType: 'FILE_SELECT',
+      modalProps: {
+        projectId: this.props.projectId,
+        model: this.props.model,
+        path: this.props.value.fileId,
+        storeKey: this.props.model, // We use the model as the storekey
+        options: {
+          allowFolder : true,
+          foldersOnly : true,
+          explore     : this.props.provider
+        },
+      },
+    })
+  },
+  render() {
+    const { provider, model, value, disabled } = this.props;
+
+    const validatePath = (path, fileId, provider) => {
+      if(provider == 'drive'){
+        return isDriveFileId(fileId) ? path : '';
+      }
+      else if(provider == 'dropbox'){
+        return isDropboxFileId(fileId) ? path : '';
+      }
+      else{
+        return ''
+      }
+    }
+
+    const path = validatePath(value.path, value.fileId, provider);
+
+    return (
+      <div className={classNames(classes.fileSelectInput, 'layout-row layout-align-start-center', {[classes.disabled] : disabled})} onClick={()=>{if(!disabled){this.showModal()}}}>
+        <div className={classes.text + ' flex'}>
+          {path ? <span><span style={{textTransform: 'capitalize'}}>{provider}/</span>{path}</span> : 'Select the project folder'}
+        </div>
+        <SimpleIconButton>
+          <MdFolder size="22" />
+        </SimpleIconButton>
+      </div>
+    );
+  }
+});
+
+function mapStateToProps() {
+  return {};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    ModalActions: bindActionCreators(ModalActions, dispatch),
+  }
+}
+
+FileSelectInput.propTypes = propTypesObject;
+
+export default connect(mapStateToProps, mapDispatchToProps)(FileSelectInput);
---
+++ electron/app/renderer/preview/pages/PreviewPage/PreviewPage.jsx
@@ -0,0 +1,96 @@
+// Container Core
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+// Component Core
+import React from 'react';
+import { has } from 'lodash';
+
+// Actions
+import * as FilesActions from 'app/shared/modules/Files/Files.actions.js';
+import * as SyncTimelineActions from 'app/shared/modules/SyncTimeline/SyncTimeline.actions.js';
+
+// Sub Components
+import LoadingOverlay   from 'app/renderer/main/components/Loading/LoadingOverlay/LoadingOverlay.jsx';
+import PreviewPageInner from './PreviewPageInner.jsx'
+import cloudMagnify    from 'app/renderer/assets/images/pure-vectors/cloud-magnify.svg';
+
+// Styles
+import classes from './PagePreview.css';
+
+///////////////////////////////// COMPONENT /////////////////////////////////
+
+export const Component = React.createClass({
+
+  // Mounting
+  onMount(nextProps, prevProps){
+    const hasFileMeta    = has(nextProps, 'fileMeta.data') && !nextProps.fileMeta.loading;
+    const string1        = prevProps ? prevProps.localPath + prevProps.projectId + prevProps.fileId + prevProps.revisionId : '';
+    const string2        = nextProps ? nextProps.localPath + nextProps.projectId + nextProps.fileId + nextProps.revisionId : '';
+    const hasChangedFile = string1 != string2;
+
+    if(string1 != string2){
+      // If localPath exists - we must get the fileId - this will get the meta
+      if(nextProps.localPath){
+        nextProps.filesActions.getMetaFromPath({
+          path       : nextProps.localPath
+        })
+      }
+      // If we do not yet have the meta, get it:
+      else if(!hasFileMeta){
+        nextProps.filesActions.getMeta({
+          projectId  : nextProps.projectId,
+          fileId     : nextProps.fileId,
+          revisionId : nextProps.revisionId
+        });
+      }
+    }
+  },
+  componentWillReceiveProps(nextProps) { this.onMount(nextProps, this.props)},
+  componentWillMount() { this.onMount(this.props) },
+  render() {
+    const { fileMeta } = this.props;
+    const hasFileMeta  = fileMeta && !fileMeta.loading && !fileMeta.data;
+
+
+    return (
+      <div className="layout-column flex">
+        { hasFileMeta ?
+          <div className="flex layout-column layout-align-center-center text-center">
+            <div style={{maxWidth: '300px'}}>
+              <img src={cloudMagnify} style={{width: '100px', height: '100px'}}/>
+              <div className="text-title-4" style={{marginBottom: '10px'}}>Could not locate this file</div>
+              <div className="text-title-5" style={{marginBottom: '20px'}}>This file could not be found in your connected cloud providers.</div>
+            </div>
+          </div>
+          : <PreviewPageInner fileMeta={fileMeta} />
+        }
+      </div>
+    );
+  }
+});
+
+///////////////////////////////// CONTAINER /////////////////////////////////
+
+function mapStateToProps({ files }, { location }) {
+  let { localPath, revisionId, fileId, projectId } = location.query;
+  fileId = fileId || (files.pathToId[localPath] ? files.pathToId[localPath].data : '');
+  const cacheKey = localPath ? fileId : `${fileId}-${revisionId}`;
+
+  return {
+    localPath,
+    fileId,
+    revisionId,
+    projectId,
+    fileMeta: files.fileMeta[cacheKey],
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    filesActions: bindActionCreators(FilesActions, dispatch),
+    syncTimelineActions: bindActionCreators(SyncTimelineActions, dispatch),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Component);
