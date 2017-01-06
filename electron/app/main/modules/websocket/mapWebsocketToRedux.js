a7853620e15106e5f1533be1c122f74aef6c9c5b
Jackson Delahunt <jackson@stemn.com>
2017-01-06 12:45:26 +1100

    new actions

--- electron/app/main/modules/websocket/mapWebsocketToRedux.js
+++ electron/app/main/modules/websocket/mapWebsocketToRedux.js
@@ -1,6 +1,8 @@
 import * as ChangesActions        from '../../../renderer/main/modules/Changes/Changes.actions.js';
 import * as TasksActions          from '../../../renderer/main/modules/Tasks/Tasks.actions.js';
 import * as ProjectActions        from '../../../shared/actions/projects.js';
+import * as ProjectActions        from '../../../shared/actions/projects.js';
+import * as SyncTimelineActions   from '../../..//shared/modules/SyncTimeline/SyncTimeline.actions.js';
 import * as NotificationsActions  from '../../../shared/modules/Notifications/Notifications.actions.js';
 import { renderFileDownload }     from '../../../shared/modules/Files/Files.actions.js';
 
@@ -29,6 +31,16 @@ export default (store, action) => {
       return (dispatch) => {
         action.payload.boards.map((boardId) => dispatch(TasksActions.getBoard({ boardId })));
       }
+    case 'TIMELINE/FETCH_FILES_TIMELINE':
+      return (dispatch) => {
+        action.payload.files.map((fileId) => dispatch(SyncTimelineActions.fetchTimeline({
+            projectId : action.payload.projectId,
+            provider : action.payload.provider,
+            fileId
+        })));
+      }
+    case 'TIMELINE/FETCH_COMMIT_TIMELINE':
+      return SyncTimelineActions.fetchTimeline({ projectId : action.payload.projectId });
     case 'BOARD/FETCH_GROUPS':
       return (dispatch) => {
         action.payload.groups.map((groupId) => dispatch(TasksActions.getGroup({ groupId, boardId : action.payload.boardId })));
@@ -42,8 +54,10 @@ export default (store, action) => {
         title : `${action.payload.user.name} Completed a Task in '${action.payload.project.name}'`,
         body  : `The task '${action.payload.task.title}' was marked as complete.`
       })
-    case 'PROJECT/FETCH_PROJECT':
-      return ProjectActions.getProject({ projectId : action.payload.projectId });
+    case 'PROJECT/FETCH_PROJECTS':
+      return (dispatch) => {
+        action.payload.projects.map((projectId) => dispatch(ProjectActions.getProject({ projectId })));
+      }
     default:
       return undefined;
   }
