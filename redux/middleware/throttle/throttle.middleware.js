1bd88cb6f8dfad8230016f0455977807b82da51f
DaveRevay2 <daverevay@gmail.com>
2017-01-08 14:11:46 +1100

    'init'

---
+++ redux/middleware/throttle/throttle.middleware.js
@@ -0,0 +1,41 @@
+/***************************************************************************************************
+
+This middleware is used to throttle actions
+
+The action should contain a throttle object such as:
+
+export function getTask({taskId}) {
+  return {
+    type: 'TASKS/GET_TASK',
+    throttle: {
+      time: 1000,                                   Time period (ms) to throttle to
+      endpoint:  `TASKS/UPDATE_TASK-${task._id}`    Unique endpoint (used to store the timeout in the timeouts object)
+    },
+    payload: {
+
+    }
+  }
+}
+
+***************************************************************************************************/
+
+import i from 'icepick';
+const timeouts = {};
+
+export default store => next => action => {
+  if(action && action.throttle && action.throttle.endpoint && action.throttle.time){
+    const { endpoint, time } = action.throttle;
+
+    if(timeouts[endpoint]){                                           // If the timeout exists:
+      clearTimeout(timeouts[endpoint])                                // Clear the timeout
+    }
+
+    timeouts[endpoint] = setTimeout(()=>{                             // Create the timeout
+      const newAction = i.assocIn(action, ['throttle'], undefined);   // Create the new action
+      next(newAction)                                                 // Dispatch the action
+    }, time)
+  }
+  else{
+    return next(action);
+  }
+};
