3515ca6e442829c86e178c2e04a68e89e8a42a2a
DaveRevay2 <daverevay@gmail.com>
2017-02-10 16:43:09 +1100

    utils

---
+++ src/misc/SyncTimeline/SyncTimeline.utils.js
@@ -0,0 +1,18 @@
+export const getRevisions = (syncTimeline) => {
+  // syncTimeline contains revision and commit events
+  // This will look inside commits and create an array of revisions.
+  const result = [];
+  
+  syncTimeline.forEach(item => {
+    if(item.event == 'revision'){
+      result.push(item)
+    }
+    else if(item.event == 'commit'){
+      item.data.items.forEach(subItem => {
+        result.push(subItem)
+      })
+    }
+  })
+  
+  return result;
+}
