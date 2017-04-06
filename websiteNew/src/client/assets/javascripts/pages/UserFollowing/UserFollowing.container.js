608d33cfc061c5e7822691fe1ae9c87853fc1162
Jackson Delahunt <jackson@stemn.com>
2017-04-06 19:16:47 +1000

    added user following

--- websiteNew/src/client/assets/javascripts/pages/UserFollowing/UserFollowing.container.js
+++ websiteNew/src/client/assets/javascripts/pages/UserFollowing/UserFollowing.container.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import fetchDataHoc from 'stemn-shared/misc/FetchDataHoc'
 
-import { getUserFollowing } from 'stemn-shared/misc/Projects/Projects.actions.js'
+import { getUserFollowing } from 'stemn-shared/misc/UserFollowing/UserFollowing.actions.js'
 
 import UserFollowing from './UserFollowing'
 
 const stateToProps = (state, { params }) => ({
   user: state.users[params.stub],
-  projects: state.projects.userProjects
+  followers: state.UserFollowing[params.stub]
 })
 
-const dispatchToProps = {}
+const dispatchToProps = {
+  getUserFollowing
+}
 
-const fetchConfigs = []
+const fetchConfigs = [{
+  hasChanged: 'params.stub',
+  onChange: (props) => {
+    props.getUserFollowing({ userId: props.params.stub })
+  }
+}]
 
 @connect(stateToProps, dispatchToProps)
 @fetchDataHoc(fetchConfigs)
--- websiteNew/src/client/assets/javascripts/pages/UserFollowing/UserFollowing.js
+++ websiteNew/src/client/assets/javascripts/pages/UserFollowing/UserFollowing.js
@@ -1,14 +1,22 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react'
 
-import classes from './UserFollowing.css';
-import classNames from 'classnames';
+import classes from './UserFollowing.css'
+import classNames from 'classnames'
+
+import User from 'modules/User'
 
 export default class UserFollowing extends Component {
+
   render() {
-    const { user } = this.props;
+    const { user, followers } = this.props
     return (
-      <div>
-        Following
+      <div className={ classes.panel }>
+        { followers && followers.data
+          ? followers.data.map((userId) => (
+            <User key={ userId } userId={ userId } className={ classes.user } />
+          ))
+          : <div>No followers found</div>
+        }
       </div>
     )
   }
