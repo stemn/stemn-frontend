65ad77f885061ccf05614169b83c9b77929d00f0
DaveRevay2 <daverevay@gmail.com>
2017-04-13 13:14:47 +1000

    Login styled

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -13,9 +13,6 @@ import SimpleIconButton from 'stemn-shared/misc/Buttons/SimpleIconButton/SimpleI
 import MdPhoto from 'react-icons/md/photo'
 import LoadingOverlay from 'stemn-shared/misc/Loading/LoadingOverlay/LoadingOverlay.jsx'
 
-import logoSvg from 'stemn-shared/assets/images/astronaut.svg'
-const backgroundImages = require.context('stemn-shared/assets/images/satellite-backgrounds')
-
 export default React.createClass({
   submit (event) {
     event.preventDefault()
@@ -27,91 +24,66 @@ export default React.createClass({
 
   render () {
     const { authenticate, nextBackground, auth } = this.props
-    const parseBackground = (bgNumber) => {
-      const total = 8
-      return bgNumber > total ? 1 : bgNumber
-    }
-    const backgroundImagePath = `./satellite-${parseBackground(auth.background)}.jpg`
-    const backgroundImage = { backgroundImage: `url(${backgroundImages(backgroundImagePath)}` }
 
-    const formPanel = (
-      <div className={classNames(classes.formPanel, 'flex', 'layout-column', 'layout-align-space-between')}>
-        <div className={classes.title + ' text-title-3'}>Sign In</div>
-        <form onSubmit={this.submit}>
-          <br />
-          <Input
-            model='auth.login.email'
-            value={auth.login.email}
-            className={classes.input}
-            type='text'
-            placeholder='Email'
-          />
-          <Input
-            model='auth.login.password'
-            value={auth.login.password}
-            className={classes.input}
-            type='password'
-            placeholder='Password'
-          />
-          <div className='layout-row layout-align-end'>
-            <div className='flex-50 layout-row'>
-              <Button style={{marginLeft: '5px'}}
-                className='primary flex'
-                type='submit'>
-                Sign In
-              </Button>
-            </div>
-          </div>
-          <div className={classes.textDivider}><div>OR</div></div>
-          <div className='layout-row'>
-            <Button onClick={() => authenticate('linkedin')}
-              style={{marginRight: '5px'}}
-              className='flex linkedin'
-              type='button'>
-              Linkedin
-            </Button>
-            <Button onClick={() => authenticate('facebook')}
-              style={{marginLeft: '5px'}}
-              className='flex facebook'
-              type='button'>
-              Facebook
-            </Button>
-          </div>
-          <LoadingOverlay show={auth.authLoading || auth.userLoading} />
-        </form>
-        <div className='layout-row'>
-          <div>Dont have an account? <Link to='/register' className='link-primary'>Register</Link></div>
-        </div>
-      </div>
-    )
-
-    const brandPanel = (
-      <div className={classNames(classes.brandPanel, 'layout-column', 'layout-align-center-center')}>
-        <img src={logoSvg} style={{width: '150px'}} draggable='false' />
-      </div>
-    )
+    const inputStyle = { marginBottom: '10px' }
 
     return (
       <div className='flex rel-box'>
-        <div className={classes.background} style={backgroundImage} />
-        <div className={classes.center + ' layout-column layout-align-center-center'}>
-          <div className={classNames(classes.mask)}>
-            <div className={classNames(classes.backgroundBlurred)} style={backgroundImage} />
+        <div className={ classNames(classes.background, 'layout-column layout-align-center-center') }>
+          <div className={ classNames(classes.panel, 'layout-column', 'layout-align-space-between') }>
+            <div className="text-title-3">Sign In</div>
+            <form onSubmit={this.submit}>
+              <br />
+              <Input
+                model="auth.login.email"
+                value={ auth.login.email }
+                className="dr-input"
+                type="text"
+                placeholder="Email"
+                style={ inputStyle }
+              />
+              <Input
+                model="auth.login.password"
+                value={ auth.login.password }
+                className="dr-input"
+                type="password"
+                placeholder="Password"
+                style={ inputStyle }
+              />
+              <div className='layout-row layout-align-end'>
+                <div className='flex-50 layout-row'>
+                  <Button
+                    style={ { marginLeft: '5px', marginTop: '5px' } }
+                    className='primary flex'
+                    type='submit'>
+                    Sign In
+                  </Button>
+                </div>
+              </div>
+              <div className={classes.textDivider}><div>OR</div></div>
+              <div className='layout-row'>
+                <Button
+                  onClick={() => authenticate('linkedin')}
+                  style={ { marginRight: '5px' } }
+                  className='flex linkedin'
+                  type='button'>
+                  Linkedin
+                </Button>
+                <Button
+                  onClick={() => authenticate('facebook')}
+                  style={ { marginLeft: '5px' } }
+                  className='flex facebook'
+                  type='button'>
+                  Facebook
+                </Button>
+              </div>
+              <LoadingOverlay show={auth.authLoading || auth.userLoading} />
+            </form>
+            <div className="layout-row text-title-5">
+              <div>Dont have an account? <Link to='/register' className='link-primary'>Register</Link></div>
+            </div>
           </div>
         </div>
-        <div className={classes.center + ' layout-column layout-align-center-center'}>
-          <div className={classNames(classes.content, 'layout-row')}>
-            {brandPanel}
-            {formPanel}
-          </div>
-        </div>
-        <SimpleIconButton
-          className={classes.nextBg}
-          onClick={nextBackground}
-          title='Change background'
-          color='white'>
-          <MdPhoto size={24} />
-        </SimpleIconButton>
       </div>
     )
   }
