fb0491a31f72f0aa8ca4f25630942012c9e45c2b
DaveRevay2 <daverevay@gmail.com>
2017-04-20 10:39:18 +1000

    Changed commits view to vertical timeline

--- websiteNew/src/client/assets/javascripts/pages/ProjectCommits/ProjectCommits.js
+++ websiteNew/src/client/assets/javascripts/pages/ProjectCommits/ProjectCommits.js
@@ -6,25 +6,13 @@ import moment from 'moment'
 import { groupBy } from 'lodash'
 
 import { Container } from 'stemn-shared/misc/Layout'
-import UserAvatar from 'stemn-shared/misc/Avatar/UserAvatar/UserAvatar'
-import Link from 'stemn-shared/misc/Router/Link'
 import LoadingOverlay from 'stemn-shared/misc/Loading/LoadingOverlay/LoadingOverlay.jsx'
 import HistoryGraph from 'stemn-shared/misc/Graphs/HistoryGraph'
-
-const groupByDay = (data) => {
-  const groupedObject = groupBy(data, item => moment(item.timestamp).format('YY/MM/DD'))
-  const groupedArray = Object.keys(groupedObject).map(key => ({
-    date: key,
-    items: groupedObject[key],
-  }))
-  return groupedArray
-}
+import TimelineVertical from 'stemn-shared/misc/TimelineVertical/TimelineVertical';
 
 export default class ProjectCommits extends Component {
   renderLoaded() {
     const { project, syncTimeline } = this.props
-    const groupedCommits = groupByDay(syncTimeline.data)
-    const getCalendarText = (time) => (moment(time).calendar().split(' at'))[0]
 
     return (
       <div>
@@ -32,44 +20,11 @@ export default class ProjectCommits extends Component {
           <div className={ classes.graphPanel }>
             <HistoryGraph />
           </div>
-          { groupedCommits.map((group) => (
-            <div>
-              <div className="text-mini-caps">
-                Commits: { getCalendarText(group.items[0].timestamp) }
-              </div>
-              <div className={ classes.commitPanel }>
-                { group.items.map(commit => (
-                  <div className={ classes.commit } key={ commit._id }>
-                    <Link name="userRoute" params={ { userId: commit.user._id } }>
-                      <UserAvatar
-                        className={ classes.avatar }
-                        name={ commit.user.name }
-                        picture={ commit.user.picture }
-                        size={ 40 }
-                        shape="square"
-                      />
-                    </Link>
-                    <div>
-                      <Link
-                        className={ classes.summary }
-                        name="commitRoute"
-                        params={ { projectId: project.data._id, commitId: commit._id } }
-                      >
-                        { commit.data.summary }
-                      </Link>
-                      <div className={ classes.meta }>
-                        <Link name="userRoute" params={ { userId: commit.user._id } }>
-                          { commit.user.name }
-                        </Link>
-                        &nbsp;commited { moment(commit.timestamp).fromNow() }.
-                      </div>
-                    </div>
-                  </div>
-                )) }
-              </div>
-            </div>
-          )) }
-
+          <TimelineVertical
+            group
+            items={ syncTimeline.data }
+            type="project"
+          />
         </Container>
       </div>
     )
