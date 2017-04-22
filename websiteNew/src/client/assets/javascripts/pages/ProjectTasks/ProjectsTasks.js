229200e84c16fa28a3d5fd508aaab782f14bda04
DaveRevay2 <daverevay@gmail.com>
2017-04-22 17:41:37 +1000

    Styles for task view

--- websiteNew/src/client/assets/javascripts/pages/ProjectTasks/ProjectsTasks.js
+++ websiteNew/src/client/assets/javascripts/pages/ProjectTasks/ProjectsTasks.js
@@ -4,10 +4,12 @@ import classes from './ProjectsTasks.css'
 import { Row, Col, Container } from 'stemn-shared/misc/Layout'
 import SubSubHeader from 'modules/SubSubHeader'
 import TaskList from 'stemn-shared/misc/Tasks/TaskList/TaskList.jsx'
-import { filterBoard } from 'stemn-shared/misc/Tasks/Tasks.utils.js'
+import TaskRow from 'stemn-shared/misc/Tasks/TaskRow'
+import { filterBoard, getAllTasks } from 'stemn-shared/misc/Tasks/Tasks.utils.js'
 import MdSearch from 'react-icons/md/search'
 import Input from 'stemn-shared/misc/Input/Input/Input'
 import Button from 'stemn-shared/misc/Buttons/Button/Button'
+import Pagination from 'stemn-shared/misc/Pagination'
 
 const SearchBox = () => (
   <div className={ classNames(classes.search, 'layout-row layout-align-start-center') }>
@@ -21,12 +23,14 @@ const SearchBox = () => (
 
 export default class ProjectsTasks extends Component {
   render() {
-    const { project, board, tasks } = this.props
+    const { project, board, tasks, location } = this.props
     const layout = 'list'
+    const page = 1
+    const noMoreResults = false
 
 
     if (board && tasks) {
-      console.log(filterBoard(board, tasks, board.searchString))
+      const filteredBoard = getAllTasks(filterBoard(board, tasks, board.searchString).data.groups)
       return (
         <div>
           <SubSubHeader>
@@ -39,9 +43,16 @@ export default class ProjectsTasks extends Component {
             </div>
           </SubSubHeader>
           <Container className={ classes.content }>
-            <div className={ classes.panel }>
-              lorem
+            <div className={ classes.threadsPanel }>
+              { filteredBoard.map(taskId => (
+                 <TaskRow
+                   board={ board }
+                   taskId={ taskId }
+                   className={ classes.thread }
+                 />
+              ))}
             </div>
+            <Pagination path={ location.pathname } page={ page } noMoreResults={ noMoreResults }/>
           </Container>
         </div>
       )
@@ -49,7 +60,9 @@ export default class ProjectsTasks extends Component {
       return null
     }
   }
-};
+}
+
+
 
 //        <div className={ classes.content + ' layout-column flex' }>
 //          <TaskList
