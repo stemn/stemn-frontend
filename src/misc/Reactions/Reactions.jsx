1497c46145234afe7c3b68beaffd66d320cc2c77
DaveRevay2 <daverevay@gmail.com>
2017-04-23 23:36:01 +1000

    Comment reaction styles

--- src/misc/Reactions/Reactions.jsx
+++ src/misc/Reactions/Reactions.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-
-// Styles
-import classNames from 'classnames';
-import classes from './Reactions.css';
-
-import Popover from 'stemn-shared/misc/Popover';
+import classNames from 'classnames'
+import classes from './Reactions.css'
+import Popover from 'stemn-shared/misc/Popover'
 import SimpleIconButton from 'stemn-shared/misc/Buttons/SimpleIconButton/SimpleIconButton.jsx'
-import MdInsertEmoticon from 'react-icons/md/insert-emoticon';
+import MdInsertEmoticon from 'react-icons/md/insert-emoticon'
 import UserAvatar from 'stemn-shared/misc/Avatar/UserAvatar/UserAvatar.jsx'
-
-import { options, groupAndOrderReactions } from './Reactions.utils.js';
-
+import { options, groupAndOrderReactions } from './Reactions.utils.js'
+import Link from 'stemn-shared/misc/Router/Link'
 
 export default React.createClass({
   render(){
@@ -18,15 +14,36 @@ export default React.createClass({
     const groupedReactions = reactions && reactions.length > 0 ? groupAndOrderReactions(reactions, options) : [];
     return (
       <span>
-        {groupedReactions.map(reaction => <Popover key={reaction.type} preferPlace="below" trigger="hover">
-          <a className={classes.icon}>{reaction.icon}</a>
-          <div className="PopoverMenu">
-            {reaction.list.map(userReaction => <div className="layout-row layout-align-start-center" style={{padding: '5px'}} key={userReaction.owner._id}>
-              <UserAvatar picture={userReaction.owner.picture} size={20}/>
-              <div style={{marginLeft: '5px'}}>{userReaction.owner.name}</div>
-            </div>)}
-          </div>
-        </Popover>)}
+        { groupedReactions.map(reaction => (
+          <Popover key={reaction.type}
+            preferPlace="below"
+            trigger="click">
+            <a className={ classes.icon }>
+              { reaction.icon }
+              <span>{ reaction.list.length }</span>
+            </a>
+            <div className="PopoverMenu">
+              { reaction.list.map(userReaction => (
+                <Link
+                  name="userRoute"
+                  params={ { userId: userReaction.owner._id } }
+                  className={ classNames(classes.listPopoverRow) }
+                  key={ userReaction.owner._id }
+                >
+                  <UserAvatar
+                    name={ userReaction.owner.name }
+                    picture={ userReaction.owner.picture }
+                    size={ 20 }
+                    shape="square"
+                  />
+                  <div style={ { marginLeft: '10px' } }>
+                    { userReaction.owner.name }
+                  </div>
+                </Link>
+              ))}
+            </div>
+          </Popover>
+        )) }
       </span>
     );
   }
