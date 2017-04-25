8af00b3b8651519776f4dace60efa4909420dabf
DaveRevay2 <daverevay@gmail.com>
2017-04-25 17:17:01 +1000

    Added open-source page

--- websiteNew/src/client/assets/javascripts/pages/OpenSource/OpenSource.js
+++ websiteNew/src/client/assets/javascripts/pages/OpenSource/OpenSource.js
@@ -8,16 +8,28 @@ import { Container } from 'stemn-shared/misc/Layout'
 import classes from './OpenSource.scss'
 
 export default class OpenSource extends Component {
+  twitterShare = () => {
+    const href = 'https://twitter.com/home?status=Great engineering is open. @Stem_Network makes @OpenEngineering easy with Dropbox and Drive integration.'
+    window.open(href, 'mywin','left=20,top=20,width=500,height=500,toolbar=1,resizable=0')
+  }
   render() {
     return (
       <LandingLayout>
-        <HeroBanner>
-          <h1>Download Stemn Desktop</h1>
-          <h3>Collaboration tools for Engineers</h3>
+        <HeroBanner style={ { minHeight: '100vh', margin: '0px' } }>
+          <div style={ { maxWidth: '680px' } }>
+            <h1>Great engineering is open.</h1>
+            <br />
+            <p>Great engineering is the result of constant iteration and relentless refinement. The best ideas grow and expand with the input of others.</p>
+            <p>Open engineering is not just about the final result. It is about sharing your process; the hours of missteps and false starts you tore through, the frustration and anguish you pushed beyond, before you arrived at that final iteration. Each of those false starts is a learning experience, a teachable moment to invite others to understand not just your solution; but how you did so.</p>
+            <p>Working in the open can be frightening. Exposing your work to critique requires bold effort. But with the collective, collaborative genius, together we can push the boundaries and create better work.</p>
+            <br />
+            <h3>Be open. Build together.</h3>
+            <div className="layout-row layout-align-center">
+              <Button style={ { margin: '0 10px'} } className="primary xl" name="loginRoute">Join the movement</Button>
+              <Button style={ { margin: '0 10px'} } className="secondary xl" onClick={ this.twitterShare }>#OpenEngineering</Button>
+            </div>
+          </div>
         </HeroBanner>
-        <Container>
-          Open Source
-        </Container>
       </LandingLayout>
     )
   }
