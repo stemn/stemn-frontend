1fdd4d2045b16b97a1fb6848151738da8534f13b
DaveRevay2 <daverevay@gmail.com>
2017-05-02 17:55:12 +1000

    Adding new task edit

--- websiteNew/src/client/assets/javascripts/pages/ProjectTask/ProjectTask.js
+++ websiteNew/src/client/assets/javascripts/pages/ProjectTask/ProjectTask.js
@@ -14,7 +14,9 @@ import MdDone from 'react-icons/md/done'
 import MdAccessTime from 'react-icons/md/access-time'
 import TaskLabelDots from 'stemn-shared/misc/Tasks/TaskLabelDots/TaskLabelDots.jsx'
 import Link from 'stemn-shared/misc/Router/Link'
-
+import Button from 'stemn-shared/misc/Buttons/Button/Button'
+import PopoverDropdown from 'stemn-shared/misc/PopoverMenu/PopoverDropdown'
+import Input from 'stemn-shared/misc/Input/Input/Input'
 
 export default class ProjectTask extends Component {
   updateTask = () => {
@@ -22,91 +24,169 @@ export default class ProjectTask extends Component {
       task: this.props.task.data
     }), 1);
   }
+  dropdownOptions = [{
+    value: 'open',
+    name: 'Status: Open',
+    onClick: () => console.log('here'),
+  }, {
+    value: 'closed',
+    name: 'Status: Closed',
+    onClick: () => console.log('here'),
+  }]
+  sidebarEdit = () => {
+    const { task, board, taskModel } = this.props
+    const group = board.data.groups.find(group => group._id === task.data.group)
+
+    return (
+      <Col className="flex-gt-xs-30 flex-order-xs-0">
+        <div className={ classes.panel }>
+          <div className="text-mini-caps">Group</div>
+          { group.name }
+        </div>
+        { task.data.labels && task.data.labels.length > 0 &&
+          <div className={ classes.panel }>
+            <div className="text-mini-caps">Labels</div>
+            <TaskLabelDots
+              labels={ task.data.labels }
+              labelInfo={ board.data.labels }
+              tag
+            />
+          </div>
+        }
+        <div className={ classes.panel }>
+          <div className="text-mini-caps">Assignees</div>
+          { task.data.users.map(user => (
+            <Link
+              name="userRoute"
+              params={ { userId: user._id } }
+              className="layout-row layout-align-start-center"
+            >
+              <UserAvatar
+                className={ classes.avatar }
+                name={ user.name }
+                picture={ user.picture }
+                size={ 30 }
+                shape='square'
+              />
+              <div>{ user.name }</div>
+            </Link>
+          ))}
+        </div>
+      </Col>
+    )
+  }
+  sidebarNonEdit = () => {
+    const { task, board } = this.props
+    const group = board.data.groups.find(group => group._id === task.data.group)
+
+    return (
+      <Col className="flex-gt-xs-30 flex-order-xs-0">
+        <div className={ classes.panel }>
+          <div className="text-mini-caps">Group</div>
+          { group.name }
+        </div>
+        { task.data.labels && task.data.labels.length > 0 &&
+          <div className={ classes.panel }>
+            <div className="text-mini-caps">Labels</div>
+            <TaskLabelDots
+              labels={ task.data.labels }
+              labelInfo={ board.data.labels }
+              tag
+            />
+          </div>
+        }
+        <div className={ classes.panel }>
+          <div className="text-mini-caps">Assignees</div>
+          { task.data.users.map(user => (
+            <Link
+              name="userRoute"
+              params={ { userId: user._id } }
+              className="layout-row layout-align-start-center"
+            >
+              <UserAvatar
+                className={ classes.avatar }
+                name={ user.name }
+                picture={ user.picture }
+                size={ 30 }
+                shape='square'
+              />
+              <div>{ user.name }</div>
+            </Link>
+          ))}
+        </div>
+      </Col>
+    )
+  }
   render() {
     const { task, project, board, taskModel, taskId, timeline } = this.props
     
     if (task && task.data && board && board.data) {
-      const group = board.data.groups.find(group => group._id === task.data.group)
+
+      const userRouteParams = {
+        userId: task.data.owner._id
+      }
+      const edit = true
 
       return (
         <div>
           <SubSubHeader>
             <h2 className={ classes.title }>
-              <span>{ task.data.name }</span>
-              <span className={ classes.number }>&nbsp;#T23</span>
+              { edit
+              ? <Input
+                  model={ `${taskModel}.data.name` }
+                  className="input-plain"
+                  value={ task.data.name }
+                />
+              : <span>{ task.data.name }</span> }
+              { edit
+              ? null
+              : <span className={ classes.number }>&nbsp;#T23</span> }
             </h2>
             <div className={ classNames('layout-row layout-align-start-center', classes.meta) }>
-              <UserAvatar
-                className={ classes.avatar }
-                name={ task.data.owner.name }
-                picture={ task.data.owner.picture }
-                size={ 20 }
-                shape='square'
-              />
-              <b>{ task.data.owner.name }</b>
+              <Link
+                name="userRoute"
+                params={ userRouteParams }
+                className="layout-row layout-align-start-center"
+              >
+                <UserAvatar
+                  className={ classes.avatar }
+                  name={ task.data.owner.name }
+                  picture={ task.data.owner.picture }
+                  size={ 20 }
+                  shape='square'
+                />
+                <b>{ task.data.owner.name }</b>
+              </Link>
               <div>&nbsp;created this thread { moment(task.data.created).fromNow() }.</div>
               <div className="flex" />
-              <Tag className="success">
-                <MdDone size={ 20 } style={ { marginRight: '5px' } }/>
-                OPEN
-              </Tag>
-              <Tag className="primary">
-                <MdAccessTime size={ 20 } style={ { marginRight: '5px' } }/>
-                {`Due ${ moment(task.data.due).fromNow() }`}
-              </Tag>
+              <PopoverDropdown
+                value={ 'open' }
+                options={ this.dropdownOptions }
+                style={ { margin: '0 15px' } }
+              />
+              { edit
+              ? <Button className="primary">
+                  Save
+                </Button>
+              : <Button className="primary">
+                  Edit
+                </Button> }
+
+
             </div>
           </SubSubHeader>
           <Container style={ { marginTop: '30px' } }>
-            <Row className="layout-row">
-              <Col className="flex">
-                <TimelineVertical 
-                  className={ classes.timeline }
-                  items={ timeline }
-                  entity={ board }
-                  type="task"
-                />
+            <Row className="layout-xs-column layout-gt-xs-row">
+              <Col className="flex flex-order-xs-1">
                 <div className={ classes.newComment }>
                   <CommentNew 
                     taskId={ taskId } 
                   />
                 </div>
-              </Col>             
-              <Col className="flex-30">
-                <div className={ classes.panel }>
-                  <div className="text-mini-caps">Group</div>
-                  { group.name }
-                </div>                
-                <div className={ classes.panel }>
-                  <div className="text-mini-caps">Labels</div>
-                  <TaskLabelDots
-                    labels={ task.data.labels }
-                    labelInfo={ board.data.labels }
-                    tag
-                  />
-                </div>                
-                <div className={ classes.panel }>
-                  <div className="text-mini-caps">Assignees</div>
-                  { task.data.users.map(user => (
-                    <Link
-                      name="userRoute"
-                      params={ { userId: user._id } }
-                      className="layout-row layout-align-start-center"
-                    >
-                      <UserAvatar
-                        className={ classes.avatar }
-                        name={ user.name }
-                        picture={ user.picture }
-                        size={ 30 }
-                        shape='square'
-                      />
-                      <div>{ user.name }</div>
-                    </Link>
-                  ))}
-                </div>
-                <div className={ classes.panel }>
-                  <div className="text-mini-caps">Related Files</div>
-                </div>
               </Col>
+              { edit
+              ? this.sidebarEdit()
+              : this.sidebarNonEdit() }
             </Row>
           </Container>
         </div>
@@ -116,6 +196,29 @@ export default class ProjectTask extends Component {
     }
   }
 }
+
+//                { timeline && timeline.length > 0 &&
+//                  <TimelineVertical
+//                    className={ classes.timeline }
+//                    items={ timeline }
+//                    entity={ board }
+//                    type="task"
+//                  />
+//                }
+
+//              <Tag className="success">
+//                <MdDone size={ 20 } style={ { marginRight: '5px' } }/>
+//                OPEN
+//              </Tag>
+//              <Tag className="primary">
+//                <MdAccessTime size={ 20 } style={ { marginRight: '5px' } }/>
+//                {`Due ${ moment(task.data.due).fromNow() }`}
+//              </Tag>
+
+//
+//                <div className={ classes.panel }>
+//                  <div className="text-mini-caps">Related Files</div>
+//                </div>
 
 //import React, { Component } from 'react'
 //import moment from 'moment'
