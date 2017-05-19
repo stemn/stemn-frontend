3f8276692559fa659ab46b91a601c03fc02f7e9d
DaveRevay2 <daverevay@gmail.com>
2017-05-19 14:15:18 +1000

    Websocket changes

--- src/misc/Files/actions/renderFile.web.js
+++ src/misc/Files/actions/renderFile.web.js
@@ -1,9 +1,10 @@
-import websocketJoinFile from './websocketJoinFile.js';
-import getUuid from 'stemn-shared/utils/getUuid.js';
+import websocketJoinFile from './websocketJoinFile.js'
 import http from 'axios';
 
 export default ({projectId, fileId, revisionId, provider, timestamp}) => (dispatch) => {
   const cacheKey = timestamp ? `${fileId}-${revisionId}-${timestamp}` : `${fileId}-${revisionId}`;
+  // Join the websocket room
+  dispatch(websocketJoinFile({renderId: cacheKey}))
   // The cache key is used as the renderId/roomId
   dispatch({
     type: 'FILES/RENDER_FILE',
@@ -21,9 +22,6 @@ export default ({projectId, fileId, revisionId, provider, timestamp}) => (dispat
     meta: {
       cacheKey,
     }
-  });
-
-  // Join the websocket room
-  dispatch(websocketJoinFile({renderId: cacheKey}))
+  })
 }
 
