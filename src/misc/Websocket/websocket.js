5b9fa96ca128faacdc57bcd08bef41793a12e48a
DaveRevay2 <daverevay@gmail.com>
2017-05-21 22:43:12 +1000

    ws log changes

--- src/misc/Websocket/websocket.js
+++ src/misc/Websocket/websocket.js
@@ -1,5 +1,10 @@
-import ws from 'ws';
-import primus from './primus-websockets2.js';
+import ws from 'ws'
+import primus from './primus-websockets2.js'
+import qs from 'querystring'
+
+const searchParams = typeof window !== 'undefined'
+  ? qs.parse(window.location.search.substring(1))
+  : {}
 
 export let socket = undefined;
 
@@ -8,7 +13,7 @@ export const initialise = (hostUrl) => {
   socket = primus.connect(hostUrl);
 
   // Log stuff
-  if (GLOBAL_ENV.NODE_ENV === 'development') {
+  if (GLOBAL_ENV.NODE_ENV === 'development' || searchParams.debug) {
     // Receive
     const socketData = (data) => {
       if (GLOBAL_ENV.APP_THREAD === 'electron') {
@@ -38,71 +43,9 @@ export const initialise = (hostUrl) => {
   socket.write({
     type: 'ADMIN/ECHO',
     payload: {
-      test: 'gooba'
+      test: 'test'
     }
   })
 
   return socket;
 }
-
-
-//import SockJs from 'sockjs-client'
-//export let socket = undefined
-//
-//export const initialise = (hostUrl) => {
-//  socket = new SockJs(hostUrl);
-////  socket.on('error', console.error )
-//
-////  // Log stuff
-////  if (GLOBAL_ENV.NODE_ENV === 'development') {
-////    // Receive
-////    const socketData = (data) => {
-////      if (GLOBAL_ENV.APP_THREAD === 'electron') {
-////        console.log(`socket | RECEIVE - ${data.type}`)
-////      } else {
-////        console.groupCollapsed(` socket | RECEIVE      ${data.type}`);
-////        console.log(data)
-////        console.groupEnd();
-////      }
-////    }
-////    socket.on('data', socketData)
-////
-////    // Write
-////    const oldWrite = socket.write.bind(socket);
-////    socket.write = (data) => {
-////      if (GLOBAL_ENV.APP_THREAD === 'electron') {
-////        console.log(`socket | SEND - ${data.type}`)
-////      } else {
-////        console.groupCollapsed(` socket | SEND         ${data.type}`);
-////        console.log(data)
-////        console.groupEnd();
-////      }
-////      return oldWrite(data)
-////    }
-////  }
-////
-////  socket.write({
-////    type: 'ADMIN/ECHO',
-////    payload: {
-////      test: 'gooba'
-////    }
-////  })
-//
-//   socket.onopen = () => {
-//     console.log('-----------------------------------------------');
-//     console.log('open');
-//     socket.send('test');
-//   }
-//
-//
-//   socket.onmessage = (e) => {
-//     console.log('message', e.data);
-//   }
-//
-//   socket.onclose = () => {
-//     console.log('-----------------------------------------------');
-//     console.log('close');
-//   };
-//
-//  return socket
-//}
