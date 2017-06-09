e29a5f884e229a35f719d92a946ee1378714544c
DaveRevay2 <daverevay@gmail.com>
2017-06-09 17:46:26 +1000

    release

--- electronNew/scripts/exec.js
+++ electronNew/scripts/exec.js
@@ -1,16 +1,22 @@
 // This scripts is used by running:
 // `node ./exec.js ./some-shell-script.sh`
 
+const bashPath = '"C:\\Program Files\\Git\\bin\\sh.exe"'
+// "C:\Program Files\Git\bin\sh.exe" --login -i -c "./release.sh"
+
 const { exec } = require('child_process')
+const fs = require('fs')
 const path = require('path')
 const [ nodePath, thisPath, scriptPath ] = process.argv
-
 const scriptFullPath = path.join(process.cwd(), scriptPath)
-
-exec(`sh ${scriptFullPath}`, (error, stdout, stderr) => {
-  console.log('stdout: ' + stdout)
-  console.log('stderr: ' + stderr)
-  if (error !== null) {
-    console.log('exec error: ' + error)
-  }
-})
+console.log(scriptFullPath);
+//exec(scriptFullPath, (error, stdout, stderr) => {
+//  console.log('stdout: ' + stdout)
+//  console.log('stderr: ' + stderr)
+//  if (error !== null) {
+//    console.log('exec error: ' + error)
+//  }
+//})
+//
+//
+//
