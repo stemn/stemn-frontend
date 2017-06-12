c33206a2fc0dc55c6f868633007acc41672d1c6b
DaveRevay2 <daverevay@gmail.com>
2017-06-12 15:46:11 +1000

    misc

--- electronNew/scripts/exec.js
+++ electronNew/scripts/exec.js
@@ -7,7 +7,7 @@ const path = require('path')
 const [ nodePath, thisPath, scriptPath ] = process.argv
 const scriptFullPath = path.join(process.cwd(), scriptPath)
 
-exec(`sh ${scriptFullPath}`, (error, stdout, stderr) => {
+exec(`bash "${scriptFullPath}"`, (error, stdout, stderr) => {
   console.log('stdout: ' + stdout)
   console.log('stderr: ' + stderr)
   if (error !== null) {
