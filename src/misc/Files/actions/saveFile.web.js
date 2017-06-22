c8503e7c6371cdaf200c4e4dda4531e2299baafa
DaveRevay2 <daverevay@gmail.com>
2017-06-22 19:32:51 +1000

    Some changes

--- src/misc/Files/actions/saveFile.web.js
+++ src/misc/Files/actions/saveFile.web.js
@@ -3,16 +3,13 @@ import http from 'axios'
 export default ({ file, fileUrl, anchorEl }) => (dispatch, getState) => {
   return http({
     method: 'GET',
-    url: 'api/v1/getTemporaryDownloadLink',
-    data: {
-      fileUrl,
-    }
+    url: `/api/v1/sync/shareLink/${file.project._id}/${file.fileId}?revisionId=${file.revisionId}`,
   }).then(({ data }) => {
     // We modify the anchor tag directly.
     // Yes, I know. Not very reacty.
     // We need this all to be sync. This seems nicer than updating the state with the new download and href attributes
     anchorEl.setAttribute('download', file.name)
-    anchorEl.setAttribute('href', data)
+    anchorEl.setAttribute('href', `${GLOBAL_ENV.API_SERVER}/api/v1/sync/shareDownload?token=${data.token}`)
     // Now, we simulate the click to start the download
     // The anchor's onClick function should check that href does not exist
     // Otherwise this will loop.
