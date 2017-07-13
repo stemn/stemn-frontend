66d40eac6cfea5dc7ca95ee4194344277d1ba8f6
Jackson Delahunt <jackson@stemn.com>
2017-07-13 20:11:25 +1000

    admin shiz

---
+++ admin/driver.js
@@ -0,0 +1,14 @@
+const Promise = require('bluebird');
+const range = require('range').range;
+
+const query = require('./index');
+
+Promise.each(range(1, 6), (number) => {
+    return query({
+        from: `2017-${number}-01`,
+        to: `2017-${number + 1}-01`,
+        filename: 'sldasm'
+    });
+}).then(() => {
+    console.log("DONE");
+});
