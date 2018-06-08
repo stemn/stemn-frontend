7c4888207004dcf25228059b6f7273461ee0eb3f
David Revay <daverevay@gmail.com>
2018-06-08 11:32:30 +1000

    Add deploy script

---
+++ stemn-frontend-website/scripts/deploy-ci.sh
@@ -0,0 +1,10 @@
+# Run this relative to stemn-frontend-website
+git clone git@github.com:stemn/stemn-website-server.git
+git config --global user.email "ci@stemn.com";
+git config --global user.name "Stemn CI"
+cd ./stemn-website-server
+rm -rf ./src/client
+cp -R ../build/client ./src/client
+git add .
+git commit -m "Automated Deployment"
+git push
