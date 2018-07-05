3e4f740eed3fe7fbfd6b279cef2147430e708e94
David Revay <daverevay@gmail.com>
2018-07-04 17:53:34 -0700

    misc

---
+++ .circleci/deploy_frontend_website.sh
@@ -0,0 +1,23 @@
+
+if [ "$CIRCLE_BRANCH" == "production" ]
+then 
+  # gcloud container clusters get-credentials stemn-beta-2018-07-01-12-09-50 --zone us-west1-c --project stemnapp
+else
+  gcloud container clusters get-credentials staging-development --zone us-east1-b --project stemnapp
+fi
+
+echo "${CIRCLE_BRANCH} - Update Website"
+
+cd ~/stemn-frontend
+
+IMAGE="gcr.io/stemnapp/${CIRCLE_BRANCH}/stemn-website-server"
+
+docker build -t "${IMAGE}:${CIRCLE_BUILD_NUM}" .
+docker build -t "${IMAGE}":latest .
+
+docker push "${IMAGE}:$TAG"
+docker push "${IMAGE}:latest"
+
+kubectl set image deployment/website-server website-server="${IMAGE}:$TAG"  
+
+echo ---------------------------
