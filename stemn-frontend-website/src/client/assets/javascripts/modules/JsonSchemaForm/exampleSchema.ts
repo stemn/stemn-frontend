006e15a2fcd5746e947378cfd988136fc184e40d
Thomas Northall-Little <tomnlittle@outlook.com>
2018-07-11 10:02:21 -0700

    cleaned up the example schema

--- stemn-frontend-website/src/client/assets/javascripts/modules/JsonSchemaForm/exampleSchema.ts
+++ stemn-frontend-website/src/client/assets/javascripts/modules/JsonSchemaForm/exampleSchema.ts
@@ -1,70 +1,85 @@
 export const schema: any = {
     "$schema": "http://json-schema.org/draft-06/schema#",
     "properties": {
-        "email_test": {
+        "string": {
+            "type": "object",
+            "title": "String field",
+            "properties": {
+                "default": {
+                    "type": "string",
+                    "title": "text input (default)"
+                },
+                "textarea": {
+                    "type": "string",
+                    "title": "textarea"
+                },
+                "email_test": {
+                    "type": "string",
+                    "format": "email"
+                },
+                "uri_test": {
+                    "type": "string",
+                    "format": "uri"
+                },
+                "color": {
+                    "type": "string",
+                    "title": "color picker",
+                    "format": "color",
+                    "default": "#151ce6"
+                },
+                "data_test": {
+                    "type": "string",
+                    "format": "data-url"
+                },
+                "date_test": {
+                    "type": "string",
+                    "format": "date"
+                },
+                "dateTime_test": {
+                    "type": "string",
+                    "format": "date-time"
+                },
+                "password_Field": {
+                    "type": "string",
+                    "format": "password"
+                },
+            }
+        },
+        "boolean": {
+            "type": "object",
+            "title": "Boolean fields",
+            "properties": {
+              "default": {
+                "type": "boolean",
+                "title": "checkbox",
+                "description": "This is the checkbox-description"
+              },
+              "radio": {
+                "type": "boolean",
+                "title": "radio buttons",
+                "description": "This is the radio-description"
+              },
+              "select": {
+                "type": "boolean",
+                "title": "select box",
+                "description": "This is the select-description"
+              }
+            }
+          },
+          "selectWidgetOptions": {
+            "title": "Custom select widget with options",
             "type": "string",
-            "format": "email"
-        },
-        "uri_test": {
-            "type": "string",
-            "format": "uri"
-        },
-        "data_test": {
-            "type": "string",
-            "format": "data-url"
-        },
-        "date_test": {
-            "type": "string",
-            "format": "date"
-        },
-        "dateTime_test": {
-            "type": "string",
-            "format": "date-time"
-        },
-        "password_Field": {
-            "type": "string",
-            "format": "password"
-        },
-        "textArea_Field": {
-            "type": "string",
-            "format": "textarea"
-        },
-        "color_Field": {
-            "type": "string",
-            "format": "color"
-        },
-        "multiple_Select": {
-            "minItems": 2,
-            "title": "A multiple-choice list",
-            "type": "array",
-            "items": {
-                "type": "string",
-                "enum": ["foo", "bar", "fuzz", "qux"],
-            },
-        },
-        "radio_select": {
-            "title": "checkbox Example",
-            "enum": ["v1", "v2", "v3"]
-        },
-        "to": {
-            "items": {
-                "type": "string"
-            },
-            "type": "array"
-        },
-        "subject": {
-            "type": "string"
-        },
-        "body": {
-            "type": "markdown"
-        },
-        "attachments": {
-            "items": {
-                "type": "string"
-            },
-            "type": "array"
-        },
+            "enum": [
+              "foo",
+              "bar"
+            ],
+            "enumNames": [
+              "Foo",
+              "Bar"
+            ]
+          }
+
     },
     "type": "object",
-    "required": ["to", "subject", "body"]
+    "required": []
 }
