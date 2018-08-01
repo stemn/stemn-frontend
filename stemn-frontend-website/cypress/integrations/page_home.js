a3d945291fac197993404860af441ac506a2778d
David Revay <daverevay@gmail.com>
2018-08-01 16:12:32 +1000

    misc

---
+++ stemn-frontend-website/cypress/integrations/page_home.js
@@ -0,0 +1,23 @@
+export default ({ url }) => {
+  describe('Home Page', () => {
+    it('Should redirect to landing', () => {
+      cy.visit(url)
+      cy.location('href').should('include', '/landing')
+    })
+    it('Should have the title and download button', () => {
+      cy.visit(url)
+      cy.contains('Seamless Version Control and Task Tracking')
+      cy.contains('Download Now')
+    })
+    it('Should have Sign in', () => {
+      cy.visit(url)
+      cy.contains('Sign in').click()
+      cy.location('href').should('include', '/login')
+    })
+    it('Should have Sign up', () => {
+      cy.visit(url)
+      cy.contains('Sign up').click()
+      cy.location('href').should('include', '/register')
+    })
+  })
+}
