c0064461276277fc680cb52598b5af5c60044a31
David Revay <daverevay@gmail.com>
2018-08-02 17:14:34 +1000

    misc

--- stemn-frontend-website/src/client/assets/javascripts/app/init/initRaven.js
+++ stemn-frontend-website/src/client/assets/javascripts/app/init/initRaven.js
@@ -1,3 +1,3 @@
 export default () => {
-  window.Raven && Raven.config('http://8c8770d2e0334ba1aafa51e90da63e65@sentry.stemn.com/7').install()
+  window.Raven && window.Raven.config('https://8c8770d2e0334ba1aafa51e90da63e65@sentry.stemn.com/7').install()
 }
