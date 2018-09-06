c3768f9aa84f616460b385ccc604686786ce3c71
David Revay <daverevay@gmail.com>
2018-09-06 14:25:17 +1000

    Fix file page

--- stemn-frontend-shared/src/misc/FileCompare/FileCompareMenu/index.js
+++ stemn-frontend-shared/src/misc/FileCompare/FileCompareMenu/index.js
@@ -1,3 +1,3 @@
 module.exports = GLOBAL_ENV.APP_TYPE === 'web'
-  ? require('./FileCompareMenu.web.jsx').default
+  ? require('./FileCompareMenu.web.jsx')
   : require('./FileCompareMenu.desktop.jsx')
--- stemn-frontend-website/src/client/assets/javascripts/pages/File/File.tsx
+++ stemn-frontend-website/src/client/assets/javascripts/pages/File/File.tsx
@@ -35,7 +35,9 @@ export interface IFileComponentProps {
   select: typeof dispatchToProps['select'],
   cacheKey: string,
   compare: any,
-  file: any,
+  file?: {
+    data?: any,
+  },
   timeline: any,
 }
 
@@ -51,7 +53,7 @@ export class FileComponent extends React.Component<IFileComponentProps, IFileCom
   public clickFileOrFolder = ({ file }) => {
     const { fileId, revisionId } = file
     const { pushRoute } = this.props
-    const projectId = this.props.file.data.project._id
+    const projectId = this.props.file && this.props.file.data.project._id
     if (file.type === 'file') {
       pushRoute(fileRoute({ fileId, projectId, revisionId }))
     } else if (projectId) {
@@ -82,26 +84,32 @@ export class FileComponent extends React.Component<IFileComponentProps, IFileCom
 
     return (
       <div className='layout-column flex' style={{ overflow: 'hidden', maxHeight: '100vh' }}>
-        <Helmet>
-          <title>{`${file.data.project.name} - ${file.data.path}`}</title>
-        </Helmet>
+        { file && file.data && (
+          <Helmet>
+            <title>{`${file.data.project.name} - ${file.data.path}`}</title>
+          </Helmet>
+        )}
         <div className={classes.header}>
-          <FileBreadCrumbs
-            className='text-ellipsis'
-            meta={file.data}
-            link={true}
-            popup={true}
-          />
+          { file && file.data && (
+            <FileBreadCrumbs
+              className='text-ellipsis'
+              meta={file.data}
+              link={true}
+              popup={true}
+            />
+          )}
           <div className='flex' />
-          <FileCompareMenu
-            cacheKey={cacheKey}
-            file1={file1}
-            file2={file2}
-            revisions={revisions}
-            mode={mode}
-            editActive={editActive}
-            changeMode={this.changeMode}
-          />
+          { file && file.data && (
+            <FileCompareMenu
+              cacheKey={cacheKey}
+              file1={file1}
+              file2={file2}
+              revisions={revisions}
+              mode={mode}
+              editActive={editActive}
+              changeMode={this.changeMode}
+            />
+          )}
           <SimpleIconButton
             className={classes.sidebarButton}
             onClick={this.toggleOpen}
@@ -112,47 +120,50 @@ export class FileComponent extends React.Component<IFileComponentProps, IFileCom
         </div>
         <div className='layout-row flex rel-box'>
           <div className={cn(classes.preview, 'layout-column flex')}>
-            <FileCompareInner
-              className='layout-column flex'
-              project={file.data.project}
-              file1={file1}
-              file2={file2}
-              mode={mode}
-              header={displayFileHeader}
-              editActive={editActive}
-            />
-            <Timeline
-              className={classes.timeline}
-              items={timelineData}
-              onSelect={this.onSelect}
-              isSelected={this.isSelected}
-              preferPlace='above'
-            />
+            {file && file.data && (
+              <FileCompareInner
+                className='layout-column flex'
+                project={file.data.project}
+                file1={file1}
+                file2={file2}
+                mode={mode}
+                header={displayFileHeader}
+                editActive={editActive}
+              />
+            )}
+            {file && file.data && (
+              <Timeline
+                className={classes.timeline}
+                items={timelineData}
+                onSelect={this.onSelect}
+                isSelected={this.isSelected}
+                preferPlace='above'
+              />
+              )}
           </div>
           <aside className={cn(classes.sidebar, { [classes.isOpen]: isOpen })}>
-            { editActive
-              ? <PipelineGraphSidebar diagramId={cacheKey} />
-              : (
-                <div>
-                  <SectionTitle className={classes.sidebarTitle}>Meta</SectionTitle>
-                  <SimpleTable>
-                    <tr><td>Name</td><td>{file.data.name}</td></tr>
-                    <tr><td>Size</td><td>{formatBytes(file.data.size)}</td></tr>
-                    <tr><td>Last modified</td><td>{moment(file.data.modified).fromNow()}</td></tr>
-                    { revisions.length > 0 &&
-                    <tr><td>Revisions</td><td>{revisions.length}</td></tr> }
-                  </SimpleTable>
-                  <AssemblyParts
-                    fileMeta={file}
-                    clickFn={this.clickFileOrFolder}
-                  />
-                  <SectionTitle className={classes.sidebarTitle}>Timeline</SectionTitle>
-                  <TimelineVertical
-                    items={timelineData}
-                    type='file'
-                  />
-                </div>
-              ) }
+            {editActive && <PipelineGraphSidebar diagramId={cacheKey} />}
+            {!editActive && file && file.data && (
+              <div>
+                <SectionTitle className={classes.sidebarTitle}>Meta</SectionTitle>
+                <SimpleTable>
+                  <tr><td>Name</td><td>{file.data.name}</td></tr>
+                  <tr><td>Size</td><td>{formatBytes(file.data.size)}</td></tr>
+                  <tr><td>Last modified</td><td>{moment(file.data.modified).fromNow()}</td></tr>
+                  { revisions.length > 0 &&
+                  <tr><td>Revisions</td><td>{revisions.length}</td></tr> }
+                </SimpleTable>
+                <AssemblyParts
+                  fileMeta={file}
+                  clickFn={this.clickFileOrFolder}
+                />
+                <SectionTitle className={classes.sidebarTitle}>Timeline</SectionTitle>
+                <TimelineVertical
+                  items={timelineData}
+                  type='file'
+                />
+              </div>
+            )}
           </aside>
         </div>
       </div>
