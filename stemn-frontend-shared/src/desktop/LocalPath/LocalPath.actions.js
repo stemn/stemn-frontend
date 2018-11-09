2fbcc6b40516aebe0a992dfa7c366c9c5c5e603a
Jackson Delahunt <jackson@stemn.com>
2018-11-09 21:18:38 +1100

    file id

--- stemn-frontend-shared/src/desktop/LocalPath/LocalPath.actions.js
+++ stemn-frontend-shared/src/desktop/LocalPath/LocalPath.actions.js
@@ -1,10 +1,11 @@
-import http from 'axios'
-export function getPath({ projectId }) {
+import http from 'axios';
+
+export function getPath({ projectId, fileId = '' }) {
   return {
     type: 'LOCAL_PATH/GET_PATH',
     payload: http({
       method: 'GET',
-      url: `/api/v1/sync/localPath/${projectId}`,
+      url: `/api/v1/sync/localPath/${projectId}/${fileId}`,
     }),
     meta: {
       projectId,
