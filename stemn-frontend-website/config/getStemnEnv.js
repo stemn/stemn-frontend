e53325fdf0a5385860cb948074933f68b375b997
Jackson Delahunt <jackson@stemn.com>
2018-12-02 15:54:29 +1100

    custom env

--- stemn-frontend-website/config/getStemnEnv.js
+++ stemn-frontend-website/config/getStemnEnv.js
@@ -2,19 +2,24 @@ require('colors')
 const { mapValues } = require('lodash')
 
 const stemnEnvs = {
+  custom: {
+    API_SERVER: process.env.API_SERVER,
+    WEBSITE_URL: process.env.WEBSITE_URL,
+    WEBSOCKET_SERVER: process.env.WEBSOCKET_SERVER,
+  },
   local: {
-    WEBSITE_URL: 'http://stemn.com',
     API_SERVER: 'http://localhost:3000',
+    WEBSITE_URL: 'http://stemn.com',
     WEBSOCKET_SERVER: 'http://localhost:8000',
   },
   staging: {
-    WEBSITE_URL: 'https://staging.stemn.com',
     API_SERVER: 'https://staging.stemn.com',
+    WEBSITE_URL: 'https://staging.stemn.com',
     WEBSOCKET_SERVER: 'https://staging.stemn.com:8443',
   },
   production: {
-    WEBSITE_URL: 'https://stemn.com',
     API_SERVER: 'https://stemn.com',
+    WEBSITE_URL: 'https://stemn.com',
     WEBSOCKET_SERVER: 'https://stemn.com:8443',
   },
 }
