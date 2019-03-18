70d14074ed305db132dfd7002bdefef695062415
Jackson Delahunt <jackson@stemn.com>
2019-03-18 15:24:45 +1100

    env

--- stemn-frontend-desktop/config/getStemnEnv.js
+++ stemn-frontend-desktop/config/getStemnEnv.js
@@ -1,21 +1,27 @@
 require('colors')
 
 const stemnEnvs = {
+  custom: {
+    API_SERVER: process.env.API_SERVER,
+    WEBSITE_URL: process.env.WEBSITE_URL,
+    WEBSOCKET_SERVER: process.env.WEBSOCKET_SERVER,
+    ELECTRON_CRASH_REPORT_SERVER: process.env.ELECTRON_CRASH_REPORT_SERVER,
+  },
   local: {
-    WEBSITE_URL: 'http://stemn.com',
     API_SERVER: 'http://localhost:3000',
+    WEBSITE_URL: 'http://stemn.com',
     WEBSOCKET_SERVER: 'http://localhost:8000',
     ELECTRON_CRASH_REPORT_SERVER: 'https://stemn.com:2096',
   },
   staging: {
-    WEBSITE_URL: 'https://staging.stemn.com',
     API_SERVER: 'https://staging.stemn.com',
+    WEBSITE_URL: 'https://staging.stemn.com',
     WEBSOCKET_SERVER: 'https://staging.stemn.com:8443',
     ELECTRON_CRASH_REPORT_SERVER: 'https://staging.stemn.com:2096',
   },
   production: {
-    WEBSITE_URL: 'https://stemn.com',
     API_SERVER: 'https://stemn.com',
+    WEBSITE_URL: 'https://stemn.com',
     WEBSOCKET_SERVER: 'https://stemn.com:8443',
     ELECTRON_CRASH_REPORT_SERVER: 'https://stemn.com:2096',
   },
